84edbd9ac7c922ee33c99543ae93a0489366cfa5
Andressguerrero <124072364+Andressguerrero@users.noreply.github.com>
2025-07-27 20:05:06 -0500

    Update archivos.py

--- archivos.py
+++ archivos.py
@@ -4,7 +4,7 @@ def convertidor_txt():
     nota = 1
     with open('notas.txt', 'w') as archivo:
         for clave, valor in app.notas.items():
-
-            archivo.write(f"--- Nota {nota} ---\n Titulo: {clave}: \n {valor}\n")
-            nota +=1
+            for fecha,contenido in clave:
+                nota +=1
+            archivo.write(f"--- Nota {nota} ---\n Titulo: {clave}: Fecha:{fecha} \n contenido: {contenido}\n ")
 convertidor_txt()
